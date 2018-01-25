3fc899953c0a46fc0f1bac81712552cb27bae39f
kkstrive <kkstrive0124@gmail.com>
2018-01-25 19:51:20 +0800

    add grid demo

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cssmin = require('gulp-minify-css');
+var webserver = require('gulp-webserver');
+
+gulp.task('watching', function () {
+    gulp.watch('grid/*.less',['less']);
+});
+
+gulp.task('less', function () {
+    gulp.src('grid/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('grid'));
+});
+
+gulp.task("cssmin",function () {
+    gulp.src('./grid/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('./grid/css'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*')
+    .pipe(gulp.dest('static/dist/img'));
+});
+
+gulp.task("web",function(){
+    gulp.src('.')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true,
+            port:8080
+        }))
+})
+gulp.task('default',['web','watching','less']);
